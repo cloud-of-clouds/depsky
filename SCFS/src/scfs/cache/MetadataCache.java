943767e4cc0960eb2bd02b1bf30387912864d5eb
Ricardo Mendes <ricardo.mends@gmail.com>
2015-06-16 17:46:33 +0100

    Deleting SCFS...

--- SCFS/src/scfs/cache/MetadataCache.java
+++
@@ -1,9 +0,0 @@
-package scfs.cache;
-
-import scfs.directoryService.DirectoryService;
-
-public interface MetadataCache extends DirectoryService {
-
-	public void removeFromCache(String metadata);
-
-}
